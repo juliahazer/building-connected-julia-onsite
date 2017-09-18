8e0fa92de3106fe9df9a5f76911fc4e5ddc40707
Admin <admin@Jons-MacBook-Pro.local>
2017-09-18 14:13:46 -0700

    working on add nested folder functionality

--- public/js/script.js
+++ public/js/script.js
@@ -21,11 +21,19 @@ $('#addFolderForm').on('submit', e => {
 		dataType: 'json',
 		contentType: 'application/json'
 	}).then(data => {
-		console.log(data);
+		//first empty the container...
+		$('#fileRowContainer').empty();
+		//then re-write root folder rows
+		getRootFiles();
 	})
 
-})
+});
 
+$('#fileRowContainer').on('click', '.addNestedFolder', e => {
+	let $currFileRow = $(e.target).closest('.fileRow')
+	let fileId = $currFileRow.attr('data-id');
+	let fileParentId = $currFileRow.attr('data-parent');
+});
 
 /*********FUNCTIONS*************/
 
@@ -35,8 +43,33 @@ function getRootFiles() {
 		type: 'GET',
 		dataType: 'JSON'
 	}).then(data => {
-		console.log(data);
-	});
+		let $fileRowContainer = $('#fileRowContainer');
+		data.forEach(file => {
+			let fileFolderClass = file.type.toLowerCase();
+			let fileSize = '';
+			if (file.size) {
+				fileSize = file.size;
+			}
+			let fileRowHtml = `
+				<div data-id="${file._id}" 
+					data-parent="${file.parentId}" 
+					class="fileRow ${fileFolderClass}">
+					<div class="fileName">
+						${file.name}
+						<span class="addNestedFolder">
+							+ Add
+						</span>
+					</div>
+					<div class="fileSize">
+						${fileSize}
+					</div>
+					<div class="fileLastMod">
+						${file.dateModified}
+					</div>
+				</div>`;
+			$fileRowContainer.append(fileRowHtml);
+		}); //end forEach
+	}); //end then
 }
 
 
