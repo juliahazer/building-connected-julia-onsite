47f234644ddc71564a68434464fd3cd3a1ea7360
Admin <admin@Jons-MacBook-Pro.local>
2017-09-18 15:58:27 -0700

    adding functionality to toggle folders open/close - need to fix bug when add new folder subfolder will not appear on toggle until refresh

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 /*********EVENT LISTENERS*************/
 
-
 $('#addFolder').on('click', e => {
 	$('#addFolderFormDiv').toggleClass('hide show-inline');
 });
@@ -47,12 +46,9 @@ $('#fileRowContainer').on('submit', '#addNestedFolderForm', e => {
 		dataType: 'json',
 		contentType: 'application/json'
 	}).then(data => {
+		console.log('here');
 		console.log(data);
 		$('.addNestedFolderDiv').remove();
-		//first empty the container...
-		//$('#fileRowContainer').empty();
-		//then re-write root folder rows
-		//getRootFiles();
 	})
 
 });
@@ -60,7 +56,7 @@ $('#fileRowContainer').on('submit', '#addNestedFolderForm', e => {
 //show/hide form to add nested form on click for +Add
 $('#fileRowContainer').on('click', '.addNestedFolder', e => {
 	let $currFileName = $(e.target).closest('.fileName');
-	let $currFileRow = $(e.target).closest('.fileRow')
+	let $currFileRow = $(e.target).closest('.fileRow');
 	let fileId = $currFileRow.attr('data-id');
 	let fileParentId = $currFileRow.attr('data-parent');
 	if ($currFileName.has('.addNestedFolderDiv').length === 0) {
@@ -77,6 +73,19 @@ $('#fileRowContainer').on('click', '.addNestedFolder', e => {
 	}
 });
 
+//clicking on folders to open / load nested one level
+$('#fileRowContainer').on('click', '.folder .fileName', e => {
+	let $currFileRow = $(e.target).closest('.fileRow');
+	let fileId = $currFileRow.attr('data-id');
+	//has the data already been retrieved/loaded as html...
+	if ($currFileRow.attr('data-subdataloaded') === "true") {
+		console.log('here');
+		$(`#fileRowContainer${fileId}`).toggleClass('show-block hide');
+	} else {
+		getNestedFiles(fileId);
+	}
+})
+
 /*********FUNCTIONS*************/
 
 function getNestedFiles(id) {
@@ -87,7 +96,8 @@ function getNestedFiles(id) {
 		dataType: 'JSON'
 	}).then(data => {
 		let $fileRow = $(`.fileRow[data-id=${id}]`);
-		$fileRow.append(`<div id="fileRowContainer${id}" data-id="${id}"></div>`);
+		$fileRow.attr('data-subdataloaded', 'true');
+		$fileRow.append(`<div id="fileRowContainer${id}" data-id="${id}" class="show-block"></div>`);
 		data.forEach(file => {
 			let fileFolderClass = file.type.toLowerCase();
 			let fileSize = '';
@@ -128,12 +138,18 @@ function getRootFiles() {
 		data.forEach(file => {
 			let fileFolderClass = file.type.toLowerCase();
 			let fileSize = '';
+			let subfolderdata = ''; //if folder, to store 
+			//if the sub data has been loaded via ajax or not (default false)
 			if (file.size) {
 				fileSize = file.size;
 			}
+			if (fileFolderClass === 'folder') {
+				subfolderdata = ' data-subdataloaded="false" ';
+			} 
 			let fileRowHtml = `
 				<div data-id="${file._id}" 
 					data-parent="${file.parentId}" 
+					${subfolderdata}
 					class="fileRow ${fileFolderClass}">
 					<div class="fileName">
 						<div class="fileNameText">
@@ -161,7 +177,5 @@ function getRootFiles() {
 $(document).ready(function() {
 
 	getRootFiles();
-
-	getNestedFiles("59c03c84ad0a9812f2faee6b"); //BE SURE TO COMMENT THIS OUT!
 
 })
