846a6913930257353f5398423f60896260a7979b
Admin <admin@Jons-MacBook-Pro.local>
2017-09-18 13:37:11 -0700

    working add folder functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,49 @@
+/*********EVENT LISTENERS*************/
+
+
+$('#addFolder').on('click', e => {
+	$('#addFolderFormDiv').toggleClass('hide show-inline');
+});
+
+$('#addFile').on('click', e => {
+	$('#addFileFormDiv').toggleClass('hide show-inline');
+});
+
+$('#addFolderForm').on('submit', e => {
+	e.preventDefault();
+	let folderName = $('#folderNameInput').val();
+	$.ajax({
+		url: '/api/folders',
+		type: 'POST',
+		data: JSON.stringify({
+			"name": folderName
+		}), 
+		dataType: 'json',
+		contentType: 'application/json'
+	}).then(data => {
+		console.log(data);
+	})
+
+})
+
+
+/*********FUNCTIONS*************/
+
+function getRootFiles() {
+	$.ajax({
+		url: '/api/files',
+		type: 'GET',
+		dataType: 'JSON'
+	}).then(data => {
+		console.log(data);
+	});
+}
+
+
+/*********CODE*************/
+
 $(document).ready(function() {
-	console.log('test');
+
+	getRootFiles();
+
 })
